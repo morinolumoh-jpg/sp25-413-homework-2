4146a763bd21fd7935ef9c8b9590d3cbb1e1f2eb
morinolumoh-jpg <morinolumoh@gmail.com>
2026-04-17 20:40:33 -0400

    Update footer.php
    Added theme metadata and custom footer content.

--- morin-theme/footer.php
+++ morin-theme/footer.php
@@ -1,6 +1,14 @@
+/*
+Theme Name: Morin Metro Theme
+Author: Omorinsola Olumoh
+Version: 1.0
+Description: A custom footer for the WordPress theme.
+*/
+
 <footer>
     <p style="text-align:center;">&copy; <?php echo date('Y'); ?> Morin Metro News. All rights reserved.</p>
 </footer>
+
 
 <?php wp_footer(); ?>
 </body>
